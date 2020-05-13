935e82187e239d7688dd9d80d8744dba36aee091
Liam Myles <liamthemonkey92@gmail.com>
2020-05-13 15:38:49 +0100

    Updates

---
+++ precache-manifest.d177f0eb6fa53c7ed3b8fa885ebacc0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39769044847a073152a96d368a4613f0",
+    "url": "/playing-with-polygons/index.html"
+  },
+  {
+    "revision": "6279ff73ea4710be7cc3",
+    "url": "/playing-with-polygons/static/css/main.d099b148.chunk.css"
+  },
+  {
+    "revision": "f3fc0c0e2698f81b491c",
+    "url": "/playing-with-polygons/static/js/2.9053da8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/playing-with-polygons/static/js/2.9053da8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6279ff73ea4710be7cc3",
+    "url": "/playing-with-polygons/static/js/main.2b11e28c.chunk.js"
+  },
+  {
+    "revision": "f75011ce768951e9c08d",
+    "url": "/playing-with-polygons/static/js/runtime-main.835b837a.js"
+  }
+]);
