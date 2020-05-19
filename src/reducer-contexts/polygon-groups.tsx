69a1aaf05530601ac00ebc4cd5a854cedc290627
Liam Myles <liamthemonkey92@gmail.com>
2020-05-19 16:40:02 +0100

    Minor changes from PR review

--- src/reducer-contexts/polygon-groups.tsx
+++ src/reducer-contexts/polygon-groups.tsx
@@ -35,7 +35,7 @@ export interface PolygonRingSides {
   colours: ReadonlyArray<string>
 }
 
-export interface PolygonRing {
+interface PolygonRing {
   active: boolean
   position: Cords
   rotation: PolygonRingRotation
