df747bb540f3b2280b7cb9d3b102e1cf5299d146
Liam Myles <liamthemonkey92@gmail.com>
2020-05-30 01:30:19 +0100

    Refactor p5 canvas to help performance
    Perviously the callback ref was leaving p5 instances running. Using this
    method seems to have a more deturmable outcome and always removes the p5 instance

--- src/components/P5Canvas.tsx
+++ src/components/P5Canvas.tsx
@@ -1,19 +1,25 @@
-import React, { useCallback } from "react"
+import React, { useEffect, useRef } from "react"
 import p5 from "p5"
 import { P5 } from "types/p5"
 
 interface P5CanvasProps {
   sketch: (p5: P5) => void
 }
+
 export const P5Canvas: React.FC<P5CanvasProps> = ({ sketch }) => {
-  const canvasRef = useCallback(
-    (node: HTMLDivElement) => {
-      if (node !== null) {
-        if (node.lastChild !== null) node.removeChild(node.lastChild)
-        new p5(sketch, node)
-      }
-    },
-    [sketch]
-  )
-  return <div ref={canvasRef} />
+  const divRef = useRef<HTMLDivElement>(null)
+  const P5Ref = useRef<P5>()
+
+  useEffect(() => {
+    const node = divRef.current as HTMLDivElement
+    const p5Instance = new p5(sketch, node)
+    P5Ref.current = p5Instance
+
+    return () => {
+      const P5Instance = P5Ref.current as P5
+      P5Instance.remove()
+    }
+  }, [P5Ref, divRef, sketch])
+
+  return <div ref={divRef} />
 }
--- src/components/__tests__/P5Canvas.test.tsx
+++ src/components/__tests__/P5Canvas.test.tsx
@@ -4,10 +4,20 @@ import p5 from "p5"
 
 import { P5Canvas } from "components/P5Canvas"
 
+const mockP5RemoveFunction = jest.fn()
+jest.mock("p5", () => {
+  return jest.fn().mockImplementation(() => {
+    return { remove: mockP5RemoveFunction }
+  })
+})
+
 describe("P5Canvas Component", () => {
-  it("should render and run sketch code", () => {
-    render(<P5Canvas sketch={() => {}} />)
+  it("should render run sketch, and remove on unmount", () => {
+    const { unmount } = render(<P5Canvas sketch={() => {}} />)
 
     expect(p5).toHaveBeenCalledTimes(1)
+    expect(mockP5RemoveFunction).not.toHaveBeenCalled()
+    unmount()
+    expect(mockP5RemoveFunction).toHaveBeenCalledTimes(1)
   })
 })
