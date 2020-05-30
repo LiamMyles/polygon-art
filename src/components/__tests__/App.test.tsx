6233e75e9c72b47ce3e2707fcafd61f4dd995652
Liam Myles <liamthemonkey92@gmail.com>
2020-05-30 01:33:03 +0100

    Updated test to require p5 to be called inside

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -2,14 +2,24 @@ import React from "react"
 import { render } from "@testing-library/react"
 
 import { NavigationContextWrapper } from "reducer-contexts/navigation"
+import { PolygonGroupsContextWrapper } from "reducer-contexts/polygon-groups"
 
 import App from "components/App"
 
+const mockP5RemoveFunction = jest.fn()
+jest.mock("p5", () => {
+  return jest.fn().mockImplementation(() => {
+    return { remove: mockP5RemoveFunction }
+  })
+})
+
 test("Renders Basic Text", () => {
   const { getByText } = render(
-    <NavigationContextWrapper>
-      <App />
-    </NavigationContextWrapper>
+    <PolygonGroupsContextWrapper>
+      <NavigationContextWrapper>
+        <App />
+      </NavigationContextWrapper>
+    </PolygonGroupsContextWrapper>
   )
   const title = getByText(/Hello/i)
   expect(title).toBeInTheDocument()
