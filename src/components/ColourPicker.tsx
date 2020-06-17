b9d3273e2d55beedeb43c9c93fe574e5a9e9a7c7
Liam Myles <liamthemonkey92@gmail.com>
2020-06-18 00:16:32 +0100

    Style colour picker

--- src/components/ColourPicker.tsx
+++ src/components/ColourPicker.tsx
@@ -1,4 +1,41 @@
 import React, { useState } from "react"
+import styled from "styled-components"
+
+const ColourPickerWrapperUl = styled.ul`
+  display: grid;
+  grid-gap: 15px;
+  grid-template-columns: repeat(25, 150px);
+  width: 100%;
+  max-width: 100vw;
+  padding: 10px 0px;
+  overflow-x: scroll;
+`
+const ColourPickerLi = styled.li`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 1fr;
+  grid-template-rows: 20px 40px 30px;
+`
+const ColourPickerLabel = styled.label`
+  text-align: center;
+  align-self: center;
+`
+const ColourPickerInput = styled.input`
+  box-sizing: border-box;
+  width: 100%;
+  height: 40px;
+  padding: 5px;
+  border-radius: 5px;
+`
+const ColourPickerDeleteButton = styled.button`
+  border-radius: 5px;
+  padding: 5px;
+`
+const ColourPickerAddButton = styled.button`
+  grid-row: 2/3;
+  width: 100%;
+  border-radius: 5px;
+`
 
 interface ColourPickerProps {
   initialColours: string[]
@@ -11,19 +48,15 @@ export const ColourPicker: React.FC<ColourPickerProps> = ({
 }) => {
   const [colours, setColours] = useState(initialColours)
   return (
-    <div>
+    <ColourPickerWrapperUl>
       {colours.map((colour, index) => {
         return (
           <React.Fragment key={`${colour}-${index}`}>
-            <form
-              onSubmit={(event) => {
-                event.preventDefault()
-              }}
-            >
-              <label htmlFor={`${index}-${colour.substr(1)}`}>
+            <ColourPickerLi>
+              <ColourPickerLabel htmlFor={`${index}-${colour.substr(1)}`}>
                 Colour {index + 1}
-              </label>
-              <input
+              </ColourPickerLabel>
+              <ColourPickerInput
                 id={`${index}-${colour.substr(1)}`}
                 type="color"
                 defaultValue={colour}
@@ -34,7 +67,8 @@ export const ColourPicker: React.FC<ColourPickerProps> = ({
                 }}
               />
               {colours.length !== 1 && (
-                <button
+                <ColourPickerDeleteButton
+                  type="button"
                   onClick={() => {
                     const newColours = [...colours]
                     newColours.splice(index, 1)
@@ -42,23 +76,26 @@ export const ColourPicker: React.FC<ColourPickerProps> = ({
                   }}
                 >
                   Delete
-                </button>
+                </ColourPickerDeleteButton>
               )}
-            </form>
+            </ColourPickerLi>
             {index + 1 === colours.length && colours.length < maxColours ? (
-              <button
-                onClick={() => {
-                  const newColours = [...colours]
-                  newColours.push("#ffffff")
-                  setColours(newColours)
-                }}
-              >
-                Add
-              </button>
+              <ColourPickerLi>
+                <ColourPickerAddButton
+                  type="button"
+                  onClick={() => {
+                    const newColours = [...colours]
+                    newColours.push("#ffffff")
+                    setColours(newColours)
+                  }}
+                >
+                  Add
+                </ColourPickerAddButton>
+              </ColourPickerLi>
             ) : null}
           </React.Fragment>
         )
       })}
-    </div>
+    </ColourPickerWrapperUl>
   )
 }
