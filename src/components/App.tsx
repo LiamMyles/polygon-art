2b4df90ff79e5d7657b9209b1ce564f518d76d75
Liam Myles <liamthemonkey92@gmail.com>
2020-07-10 02:51:11 +0100

    Added spme styling to edit background modal

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -54,6 +54,22 @@ const ColourPickerInput = styled.input`
   width: 100%;
   height: 35px;
   padding: 5px;
+  margin: 5px 0 0;
+  border-radius: 5px;
+`
+
+const ModalInternalWrappingDiv = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  font-size: 20px;
+`
+
+const ModalUpdateButton = styled.button`
+  display: block;
+  width: 80%;
+  height: 50px;
+  margin: 10px auto 0;
+  font-size: 18px;
   border-radius: 5px;
 `
 
@@ -94,46 +110,50 @@ const EditBackgroundModal: React.FC = () => {
       isClosed={editModalIsClosed}
       setIsClosed={setEditModalIsClosed}
     >
-      <ToggleSwitch
-        checked={shouldRedrawBackground}
-        id="redraw-background-toggle"
-        label="Redraw Background"
-        setFunction={setShouldRedrawBackground}
-        checkedText={{ checked: "Yes", unchecked: "No" }}
-      />
-      <label htmlFor="background-colour-picker">Colour</label>
-      <ColourPickerInput
-        id="background-colour-picker"
-        type="color"
-        value={backgroundHex}
-        onChange={({ currentTarget: { value } }) => {
-          setBackgroundHex(value)
-        }}
-      />
-      <Slider
-        label="Opacity"
-        id="background-opacity"
-        currentValue={backgroundOpacity}
-        max={100}
-        min={0}
-        setFunction={setBackgroundOpacity}
-      />
-      <button
-        type="button"
-        disabled={!canUpdate}
-        onClick={() => {
-          backgroundDispatch({
-            type: "UPDATE_BACKGROUND_WITH_HEX",
-            shouldRedraw: shouldRedrawBackground,
-            hexColour: backgroundHex,
-            opacity: backgroundOpacity,
-          })
-          setCanUpdate(false)
-          setEditModalIsClosed(true)
-        }}
-      >
-        Update
-      </button>
+      <ModalInternalWrappingDiv>
+        <ToggleSwitch
+          checked={shouldRedrawBackground}
+          id="redraw-background-toggle"
+          label="Redraw Background"
+          setFunction={setShouldRedrawBackground}
+          checkedText={{ checked: "Yes", unchecked: "No" }}
+        />
+        <div>
+          <label htmlFor="background-colour-picker">Colour</label>
+          <ColourPickerInput
+            id="background-colour-picker"
+            type="color"
+            value={backgroundHex}
+            onChange={({ currentTarget: { value } }) => {
+              setBackgroundHex(value)
+            }}
+          />
+        </div>
+        <Slider
+          label="Opacity"
+          id="background-opacity"
+          currentValue={backgroundOpacity}
+          max={100}
+          min={0}
+          setFunction={setBackgroundOpacity}
+        />
+        <ModalUpdateButton
+          type="button"
+          disabled={!canUpdate}
+          onClick={() => {
+            backgroundDispatch({
+              type: "UPDATE_BACKGROUND_WITH_HEX",
+              shouldRedraw: shouldRedrawBackground,
+              hexColour: backgroundHex,
+              opacity: backgroundOpacity,
+            })
+            setCanUpdate(false)
+            setEditModalIsClosed(true)
+          }}
+        >
+          Update
+        </ModalUpdateButton>
+      </ModalInternalWrappingDiv>
     </ModalBox>
   )
 }
